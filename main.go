761c2340be9c9642df9b67db3c2df58c104fd587
zeqing-guo <zqguo92@gmail.com>
2017-08-01 06:27:01 +0000

    [ADD] deploy and delete template
    	new file:   AzureBlockchainBroker
    	modified:   broker/azure.go
    	modified:   broker/config.go
    	modified:   glide.lock
    	modified:   main.go
    	new file:   util/util.go
    	modified:   vendor/github.com/Azure/go-autorest/.travis.yml
    	modified:   vendor/github.com/Azure/go-autorest/autorest/retriablerequest.go
    	new file:   vendor/github.com/Azure/go-autorest/autorest/utils/auth.go
    	new file:   vendor/github.com/Azure/go-autorest/autorest/utils/commit.go
    	new file:   vendor/github.com/fatih/structs/.gitignore
    	new file:   vendor/github.com/fatih/structs/.travis.yml
    	new file:   vendor/github.com/fatih/structs/LICENSE
    	new file:   vendor/github.com/fatih/structs/README.md
    	new file:   vendor/github.com/fatih/structs/field.go
    	new file:   vendor/github.com/fatih/structs/field_test.go
    	new file:   vendor/github.com/fatih/structs/structs.go
    	new file:   vendor/github.com/fatih/structs/structs_example_test.go
    	new file:   vendor/github.com/fatih/structs/structs_test.go
    	new file:   vendor/github.com/fatih/structs/tags.go
    	new file:   vendor/github.com/fatih/structs/tags_test.go

--- main.go
+++ main.go
@@ -1,21 +1,78 @@
 package main
 
 import (
+	"flag"
+	// "fmt"
 	"os"
 
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagerflags"
 	// "github.com/Azure/go-autorest/autorest"
 	// "github.com/Azure/go-autorest/autorest/adal"
-	"github.com/zeqing-guo/AzureBlockchainBroker/azureblockchainbroker"
+	"github.com/zeqing-guo/AzureBlockchainBroker/broker"
 )
 
 func main() {
+	parseCommandLine()
 	sink, err := lager.NewRedactingWriterSink(os.Stdout, lager.INFO, nil, nil)
 	if err != nil {
 		panic(err)
 	}
-	logger, logsink := lagerflags.NewFromSink("azureblockchainbroker", sink)
-	logger.Info("starting")
+	logger, _ := lagerflags.NewFromSink("azureblockchainbroker", sink)
+	logger.Info("start")
 	defer logger.Info("end")
+
+	azureConfig := broker.NewAzureConfig(
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+	)
+	azureStackConfig := broker.NewAzureStackConfig("", "", "", "")
+	cloudConfig := broker.NewCloudConfig(*azureConfig, *azureStackConfig)
+	configuration := broker.NewConfiguration(
+		"c4528d9e-c99a-48bb-b12d-fde2176a43b8",
+		"binxi-zeqing-testsb1",
+		true,
+		"southcentralus",
+		"",
+		false,
+		false,
+	)
+	blockchainConfig := broker.NewBlockchainConfiguration(
+		"ethnet",
+		"",
+		"",
+		"",
+		"",
+		23219,
+		2,
+		1,
+		"Standard_A1",
+		1,
+		"Standard_A1",
+	)
+	account, err := broker.NewAzureAccount(logger, *cloudConfig, *configuration, *blockchainConfig)
+	if err != nil {
+		panic(err)
+	}
+	// err = account.Create()
+	// res := <-account.DeploymentResult
+	// logger.Info("deployment-response-data", lager.Data{
+	// 	"Parameters":     res.Properties.Parameters,
+	// 	"ParametersLink": res.Properties.ParametersLink,
+	// })
+	err = account.Create()
+	if err != nil {
+		panic(err)
+	}
+	// logger.Info("Deployment-Get", lager.Data{
+	// "de": de,
+	// })
+}
+
+func parseCommandLine() {
+	lagerflags.AddFlags(flag.CommandLine)
 }
